fc08ca4f2bdf2eefd049dfff61902044ae30d4f6
huynhtri998 <huynhtri998@gmail.com>
2026-06-03 23:01:45 +0700

    practice React Query

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -9,10 +9,21 @@ import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
 import AppLayout from "./ui/AppLayout.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -29,7 +40,7 @@ function App() {
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers.js";
+import { deleteCabin } from "../../services/apiCabins.js";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +41,42 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+
+      alert("Cabin deleted successfully");
+    },
+    onError: (err) => {
+      alert(err.message);
+    },
+  });
+
+  return (
+    <TableRow role={"row"}>
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{discount ? formatCurrency(discount) : "—"}</Discount>
+      <button onClick={() => mutate(cabinId)}>Delete</button>
+    </TableRow>
+  );
+}
+export default CabinRow;
