326bb68a087ab715f93443d92847dfa680466607
huynhtri998 <huynhtri998@gmail.com>
2026-06-24 17:03:54 +0700

    practice Project: Add search, filter, sort booking

--- the-wild-oasis/src/services/apiBookings.js
+++ the-wild-oasis/src/services/apiBookings.js
@@ -96,12 +96,27 @@ export async function deleteBooking(id) {
   return data;
 }
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy } = {}) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
     );
+
+  // FILTER
+  if (filter) {
+    const method = filter.method || "eq";
+    query = query[method](filter.field, filter.value);
+  }
+
+  // SORT
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+  }
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
